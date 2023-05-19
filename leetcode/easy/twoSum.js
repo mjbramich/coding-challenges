e3d40d883d38e2da2d514bbb99513ac647c56129
Matthew Bramich <matt_bramich@hotmail.com>
2023-05-19 16:45:59 +1000

    solution for twoSum

---
+++ leetcode/easy/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ // function to see when 2 numbers in an array = target
+var twoSum = function(nums, target) {
+    //nums = array of numbers, target = integer
+    // is grabbing the first number
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(j !== i && nums[i] + nums[j] === target){
+                return [i,j]
+            }
+        }
+    }
+    // return the index of the 2 numbers. in an array
+};
