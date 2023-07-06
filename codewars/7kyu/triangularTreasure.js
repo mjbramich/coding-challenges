85434385411c12a1bfe93643b5b7d7e13ad200da
Matthew Bramich <matt_bramich@hotmail.com>
2023-07-07 09:22:06 +1000

    solution for triangularTreasure

---
+++ codewars/7kyu/triangularTreasure.js
@@ -0,0 +1,33 @@
+/* Description
+Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+1st (1)   2nd (3)    3rd (6)
+*          **        ***
+           *         **
+                     *
+You need to return the nth triangular number. You should return 0 for out of range values:
+
+For example: (Input --> Output)
+
+0 --> 0
+2 --> 3
+3 --> 6
+-10 --> 0
+*/
+
+// My solution
+
+// Return the nth triangular number
+function triangular(n) {
+    if (n <= 0) {
+        return 0;
+    }
+
+    return (n * (n + 1)) / 2;
+
+    // so make a pyramid shape
+    // eg 1 - 2 - 3
+    //   *
+    //  * *
+    // * * *
+}
