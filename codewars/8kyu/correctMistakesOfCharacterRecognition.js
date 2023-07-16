69a943601f7e24325e92ca9cc69a4f58b5f10f1b
Matthew Bramich <matt_bramich@hotmail.com>
2023-07-16 10:40:54 +1000

    solution for correctMistakesOfCharacterRecognition

---
+++ codewars/8kyu/correctMistakesOfCharacterRecognition.js
@@ -0,0 +1,29 @@
+/* Description
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+*/
+
+// My solution
+function correct(string) {
+	let newWord = string;
+
+	if (newWord.includes(5)) {
+		newWord = newWord.replaceAll('5', 'S');
+	}
+	if (newWord.includes(0)) {
+		newWord = newWord.replaceAll(0, 'O');
+	}
+	if (newWord.includes(1)) {
+		newWord = newWord.replaceAll(1, 'I');
+	}
+
+	return newWord;
+}
