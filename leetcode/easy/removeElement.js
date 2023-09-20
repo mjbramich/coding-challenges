e49fa28fcf65bb2ca78a7527ac8ccc955318f649
Matthew Bramich <matt_bramich@hotmail.com>
2023-09-21 06:19:05 +1000

    solution for concatenationOfArray

--- leetcode/easy/removeElement.js
+++ leetcode/easy/removeElement.js
@@ -62,11 +62,24 @@ var removeElement = function (nums, val) {
 		if (nums[right] !== val) {
 			nums[left] = nums[right];
 			left++;
-			s;
 		}
 
 		right++;
 	}
 
 	return left;
+};
+
+var removeElement = function (nums, val) {
+	if (nums.length === 0) {
+		return 0;
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] === val) {
+			nums.splice(i, 1);
+			i--;
+		}
+	}
+	return nums.length;
 };
