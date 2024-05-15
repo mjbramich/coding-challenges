1561b1854d41b753fe182626459e680d2f647f5d
matt <matt_bramich@hotmail.com>
2024-05-16 07:47:30 +1000

    solution for convertBooleanToString

---
+++ codewars/8kyu/convertBooleanToString.ts
@@ -0,0 +1,11 @@
+/* Description 
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+// My Solution
+export const boolToWord = (bool: boolean): string => {
+  // Based on input(boolean) return "Yes" : 'No'
+  return bool === true ? "Yes" : "No";
+};
