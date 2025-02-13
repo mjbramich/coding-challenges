f034913ebc731a98dd9f1146eb663d7dbf8bdf96
mjbramich <matt_bramich@hotmail.com>
2025-02-14 06:38:47 +1100

    solution for reverseWordsInAString

---
+++ codewars/8kyu/reverseWordsInAString.js
@@ -0,0 +1,11 @@
+/* Description: 
+You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi"
+*/
+
+// My solution:
+const reverse = (str) => str.split(" ").reverse().join(" ");
