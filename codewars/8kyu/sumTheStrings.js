aaf4523357d321879830e9bc6077269b8c6e454e
mjbramich <matt_bramich@hotmail.com>
2025-02-15 06:55:48 +1100

    solution for sumTheStrings

---
+++ codewars/8kyu/sumTheStrings.js
@@ -0,0 +1,20 @@
+/* Description:
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+
+Notes:
+If either input is an empty string, consider it as zero.
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+*/
+
+// My solution
+//This handles case where a or b is an empty string since due to coercion, Number('') returns 0
+const sumTheStrings = (a, b) => {
+  return `${Number(a) + Number(b)}`;
+};
