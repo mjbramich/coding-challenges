c3d0a2d7935562cc33f780c0a807ac8a0a522d00
mjbramich <matt_bramich@hotmail.com>
2025-02-22 07:48:28 +1100

    solution for breakCamelCase

--- codewars/6kyu/breakCamelCase.js
+++ codewars/6kyu/breakCamelCase.js
@@ -1,14 +1,36 @@
+/*Description:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
 function solution(string) {
-    
-    let result = ''
+  let result = "";
 
-    for(let i = 0; i < string.length; i++){
-
-        if(string[i] === string[i].toUpperCase()){
-            result += ' '
-        }
-        result += string[i]
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      result += " ";
     }
-    return result
+    result += string[i];
+  }
+  return result;
 }
 
+// Solution revisited:
+
+function solution(string) {
+  let res = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      res += ` ${string[i]}`;
+      i++;
+    }
+    res += string[i];
+  }
+
+  return res;
+}
