e2df4bf8972e370d4f21a0ffbc8a1da390a9783c
mjbramich <matt_bramich@hotmail.com>
2025-02-25 08:48:09 +1100

    solution for arrayDiff

--- codewars/6kyu/arrayDiff.js
+++ codewars/6kyu/arrayDiff.js
@@ -9,14 +9,16 @@ If a value is present in b, all of its occurrences must be removed from the othe
 arrayDiff([1,2,2,2,3],[2]) == [1,3]
 */
 
-
 // My solution
 function arrayDiff(a, b) {
-    // input is 2 arrays, a = list of values, b = values to remove from array a 
-    // create a new array with filtered values not present in array b
-    let c = a.filter((v) => !b.includes(v))
-    
-    return c 
-  }
+  // input is 2 arrays, a = list of values, b = values to remove from array a
+  // create a new array with filtered values not present in array b
+  let c = a.filter((v) => !b.includes(v));
 
-  
+  return c;
+}
+
+// Revisited Solution
+function array_diff(a, b) {
+  return a.filter((e) => !b.includes(e));
+}
