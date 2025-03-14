884c76c411ae1239763077ac03612fc52cd513d6
mjbramich <matt_bramich@hotmail.com>
2025-03-15 07:05:33 +1100

    solution for simpleDistinct

---
+++ codewars/8kyu/simpleDistinct.sql
@@ -0,0 +1,14 @@
+/* Description
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+people table schema
+id
+name
+age
+select table schema
+age (distinct)
+*/
+
+
+-- My solution
+SELECT DISTINCT Age FROM PEOPLE;
