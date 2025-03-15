85c349bd263e447cc9018f8a3c5843cede0ce31d
mjbramich <matt_bramich@hotmail.com>
2025-03-15 15:19:43 +1100

    solution for superDuperEasy

---
+++ codewars/8kyu/superDuperEasy.py
@@ -0,0 +1,12 @@
+"""
+Description:
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+
+"""
+
+# My solution
+
+def problem(a):
+    if type(a) == str: return "Error"
+    return a * 50 + 6
