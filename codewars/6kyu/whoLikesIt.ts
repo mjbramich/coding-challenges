605f83dd4bfccc9882385fa1d95f809963cde89f
mjbramich <matt_bramich@hotmail.com>
2025-03-28 09:02:20 +1100

    solution for whoLikesIt

---
+++ codewars/6kyu/whoLikesIt.ts
@@ -0,0 +1,30 @@
+/* Description:
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+
+// My solution:
+
+export const likes = (a: string[]) => {
+  const people = a.length;
+
+  switch (people) {
+    case 0:
+      return `no one likes this`;
+    case 1:
+      return `${a[0]} likes this`;
+    case 2:
+      return `${a[0]} and ${a[1]} like this`;
+    case 3:
+      return `${a[0]}, ${a[1]} and ${a[2]} like this`;
+    default:
+      return `${a[0]}, ${a[1]} and ${people - 2} others like this`;
+  }
+};
