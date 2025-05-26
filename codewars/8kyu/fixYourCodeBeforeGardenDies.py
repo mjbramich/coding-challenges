154b916718b3d491f072a0aa85b3bbf5ec05bc07
mjbramich <matt_bramich@hotmail.com>
2025-05-27 07:46:59 +1000

    solution for fixYourCodeBeforeGardenDies

---
+++ codewars/8kyu/fixYourCodeBeforeGardenDies.py
@@ -0,0 +1,14 @@
+# Description:
+# You have an award-winning garden and every day the plants need exactly 40mm of water. 
+# You created a great piece of JavaScript to calculate the amount of water your plants will need when 
+# you have taken into consideration the amount of rain water that is forecast for the day. 
+# Your jealous neighbour hacked your computer and filled your code with bugs.
+
+
+
+# My Solution
+def rain_amount(mm):
+    if (mm < 40):
+         return f"You need to give your plant {40-mm}mm of water"
+    else:
+         return "Your plant has had more than enough water for today!"
