c238b9b297aec6ea7c394ce0d13fa095480b7eff
mjbramich <matt_bramich@hotmail.com>
2025-05-30 08:45:06 +1000

    solution for pillars

--- codewars/8kyu/pipeProblem.py
+++ codewars/8kyu/pipeProblem.py
@@ -1,3 +1,8 @@
+# Description:
+# Given a list of unique numbers sorted in ascending order, 
+# Return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+# Example Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
 # My solution: 
 def pipe_fix(nums):
     min = nums[0]
