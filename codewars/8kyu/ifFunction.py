8b9ec17513563dd99874997377aa71f16c06dc60
mjbramich <matt_bramich@hotmail.com>
2025-06-13 09:03:23 +1000

    solution for ifFunction

---
+++ codewars/8kyu/ifFunction.py
@@ -0,0 +1,9 @@
+# Description: 
+# Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+# When bool is truthy, func1 should be called, otherwise call the func2.
+
+
+
+# My solution:
+def _if(bool, func1, func2):
+    return func1() if bool else func2()
